36d220c31aa243dcd69733fbab88786f0d30434e
Vishal <vkchouksey14@gmail.com>
2022-07-14 08:43:11 +0100

    Initial commit

---
+++ src/main/java/com/example/libraryservicemanagement/LibraryServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryservicemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryServiceManagementApplication.class, args);
+    }
+
+}
